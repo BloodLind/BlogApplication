2c3f4bd56962c3c283a2ae56d7b8ea805bfd793c
BloodLind <pashtet.sadist@gmail.com>
2021-11-15 18:49:40 +0300

    Login stage 1

--- blog-client/src/components/login.jsx
+++ blog-client/src/components/login.jsx
@@ -1,5 +1,5 @@
 
-import '../styles/Login.css';
+import '../styles/login.css';
 import React from "react";
 import {
   BrowserRouter as Router,
@@ -13,15 +13,13 @@ import useSession from 'react-session-hook';
 import { useState,useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import '../styles/site.css'
+
+
+
 function Login (){
- 
- 
-   
-        const [state,setState]  = useState( {login: '',password:''})
-    
-      const session=  useSession();
-      const history = useHistory();
-     
+    const [state,setState]  = useState( {login: '',password:''})
+    const session=  useSession();
+    const history = useHistory();
     const handleChange = (event)=> {    
        setState({...state,[event.target.name]:event.target.value});
     };
@@ -43,27 +41,24 @@ function Login (){
       return (
        
        <div className="base" style={{backgroundImage: "url(/drawable/loginBack.png)"}}>
-          <div className="loginContainer">
-            <div className="loginForm">
-          <input className="input" type="text" placeholder="Login" name="login" value={state.login} onChange={handleChange} />
-          <input className="input" type="password" placeholder="Password" name="password" value={state.password}  onChange={handleChange} />
-          <div class="">
-          <input className="yellowBtn" type="button" onClick={handleSingIn} value="Sign in"/>
-          <input className="yellowBtn" type="button" onClick={handleSingUp} value="Sing up"></input>
-          </div>
-          <div>Forgot password?</div>
-          <div className="blackBtn">
-            Connect with Google</div>
-        
+          <div className="loginContainer col-5 d-flex flex-row align-items-center">
+            <div className="loginForm p-2 justify-content-center align-items-center">
+              <input className="input w-100" type="text" placeholder="Login" name="login" value={state.login} onChange={handleChange} />
+              <input className="input w-100" type="password" placeholder="Password" name="password" value={state.password}  onChange={handleChange} />
+              <div class="d-flex gap-5">
+                <button className="btn bg-accent text-larger agency ps-5 pe-5" type="button" onClick={handleSingIn}>Sign in</button>
+                <button className="btn bg-accent text-larger agency ps-5 pe-5" type="button" onClick={handleSingUp}>Sign up</button>
+              </div>
+              <Link className="nav-link">Forgot password?</Link>
+              
+              <button className="btn bg-dark-color color-sub-light w-100 p-2">Connect with Google</button>
           </div>
 
-          <div className="logo">
-            <img style={{height:"300px"}} src="/drawable/logowithtextdark.png"/>
-          
-          </div>
-          </div>
+              <div className="logo">
+                <img style={{height:"300px"}} src="/drawable/logowithtextdark.png"/>
+              </div>
        </div>
-     
+     </div>
       );
     
   }
