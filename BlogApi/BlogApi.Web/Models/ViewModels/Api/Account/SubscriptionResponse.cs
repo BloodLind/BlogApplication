2ef6cfa1bf095dde842cde16c3accab501401917
BloodLind <pashtet.sadist@gmail.com>
2021-11-15 14:32:05 +0300

    Client Updated

--- BlogApi/BlogApi.Web/Models/ViewModels/Api/Account/SubscriptionResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BlogApi.Web.Models.ViewModels.Api.Account
-{
-    public class SubscriptionResponse
-    {
-        public string AuthorId { get; set;  }
-        public string SubscriberId { get; set;  }
-    }
-}
--- BlogApi/BlogApi.Web/Models/ViewModels/Api/Blog/SubcriptionArticlesRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BlogApi.Web.Models.ViewModels.Api.Blog
-{
-    public class SubscriptionArticlesRequest
-    {
-        public string UserLogin { get; set; }
-        public int Page { get; set; }
-    }
-}
