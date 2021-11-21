198f24ee3f4a8cc1dfbead5a8153ae49a2fd8ac6
abeba <sashok.supero@gmail.com>
2021-11-21 19:00:48 +0200

    Styles divided to different files

--- blog-client/src/components/login.jsx
+++ blog-client/src/components/login.jsx
@@ -1,5 +1,6 @@
 
 import '../styles/login.css';
+import '../styles/default-namespace.jsx'
 import React from "react";
 import {
   BrowserRouter as Router,
@@ -10,57 +11,57 @@ import {
 
 import AccountService from "../api/accountService";
 import useSession from 'react-session-hook';
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import '../styles/site.css'
 
 
 
-function Login (){
-    const [state,setState]  = useState( {login: '',password:''})
-    const session=  useSession();
-    const history = useHistory();
-    const handleChange = (event)=> {    
-       setState({...state,[event.target.name]:event.target.value});
-    };
-   const handleSingIn = (event)=> {
-    AccountService.Login( state.login,state.password,session);
+function Login() {
+  const [state, setState] = useState({ login: '', password: '' })
+  const session = useSession();
+  const history = useHistory();
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.value });
+  };
+  const handleSingIn = (event) => {
+    AccountService.Login(state.login, state.password, session);
     history.push("/")
-   }
-  
-  const handleSingUp = (event)=>{
-    alert('data ' + state.login + "   "+state.password);
-    
   }
-//   this.handleChange = this.handleChange.bind(this);
-//   this.handleSingIn = this.handleSingIn.bind(this);
-//   this.handleSingUp = this.handleSingUp.bind(this);
 
-  
+  const handleSingUp = (event) => {
+    alert('data ' + state.login + "   " + state.password);
 
-      return (
-       
-       <div className="base" style={{backgroundImage: "url(/drawable/loginBack.png)"}}>
-          <div className="loginContainer col-5 d-flex flex-row align-items-center">
-            <div className="loginForm p-2 justify-content-center align-items-center">
-              <input className="input w-100" type="text" placeholder="Login" name="login" value={state.login} onChange={handleChange} />
-              <input className="input w-100" type="password" placeholder="Password" name="password" value={state.password}  onChange={handleChange} />
-              <div class="d-flex gap-5">
-                <button className="btn bg-accent text-larger agency ps-5 pe-5" type="button" onClick={handleSingIn}>Sign in</button>
-                <button className="btn bg-accent text-larger agency ps-5 pe-5" type="button" onClick={handleSingUp}>Sign up</button>
-              </div>
-              <Link className="nav-link">Forgot password?</Link>
-              
-              <button className="btn bg-dark-color color-sub-light w-100 p-2">Connect with Google</button>
+  }
+  //   this.handleChange = this.handleChange.bind(this);
+  //   this.handleSingIn = this.handleSingIn.bind(this);
+  //   this.handleSingUp = this.handleSingUp.bind(this);
+
+
+
+  return (
+
+    <div className="base" style={{ backgroundImage: "url(/drawable/loginBack.png)" }}>
+      <div className="loginContainer col-5 d-flex flex-row align-items-center">
+        <div className="loginForm p-2 justify-content-center align-items-center">
+          <input className="input w-100" type="text" placeholder="Login" name="login" value={state.login} onChange={handleChange} />
+          <input className="input w-100" type="password" placeholder="Password" name="password" value={state.password} onChange={handleChange} />
+          <div class="d-flex gap-5">
+            <button className="btn bg-accent text-larger agency ps-5 pe-5" type="button" onClick={handleSingIn}>Sign in</button>
+            <button className="btn bg-accent text-larger agency ps-5 pe-5" type="button" onClick={handleSingUp}>Sign up</button>
           </div>
+          <Link className="nav-link">Forgot password?</Link>
 
-              <div className="logo">
-                <img style={{height:"300px"}} src="/drawable/logowithtextdark.png"/>
-              </div>
-       </div>
-     </div>
-      );
-    
-  }
+          <button className="btn bg-dark-color color-sub-light w-100 p-2">Connect with Google</button>
+        </div>
+
+        <div className="logo">
+          <img style={{ height: "300px" }} src="/drawable/logowithtextdark.png" />
+        </div>
+      </div>
+    </div>
+  );
+
+}
 
 export default Login;
