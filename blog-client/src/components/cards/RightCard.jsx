da27d47fd3301a82c4600b67922ed4309e3e4226
BloodLind <pashtet.sadist@gmail.com>
2021-12-13 17:02:03 +0200

    Fixed some bugs on different browsers

--- blog-client/src/components/cards/RightCard.jsx
+++ blog-client/src/components/cards/RightCard.jsx
@@ -22,7 +22,6 @@ function RightCard(props) {
                 <path d="M8.956,334.69H665.451a8.5,8.5,0,0,0,8.227-8.749h0c0-4.832.486-137.155,0-224.449L69,73.594C43.753,72.638,21.187,69.68.034,86.421l.695,239.52A8.5,8.5,0,0,0,8.956,334.69Z" transform="translate(-0.034 -72.772)" fill="#191919" opacity="0.9"
                     style={
                         {
-                            mixBlendMode: 'multiply',
                             isolation: 'isolate'
                         }} />
             </svg>
